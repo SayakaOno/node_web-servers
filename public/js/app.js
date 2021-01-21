9ff1fd94c2340d5b5b993b86c4fa049111769912
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-20 20:54:22 -0800

    implement browser HTTP request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('client side javascript file is loaded');
+
+fetch('/weather?address=boston').then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location, data.forecast);
+    }
+  });
+});
