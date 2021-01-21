1815593da77f40550cba952f1d63c763354d9a2c
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-20 21:12:35 -0800

    render API response to the browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,26 @@
 console.log('client side javascript file is loaded');
 
-fetch('/weather?address=boston').then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location, data.forecast);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch(`/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
   });
 });
